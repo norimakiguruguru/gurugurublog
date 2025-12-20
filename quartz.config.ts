eb8a7eb18d3a1a9ec5ccbd9d2e9ae4b1d33c5f53
norimakiguruguru <noriaki.miyazaki.0927@gmail.com>
2025-12-20 10:50:29 +0900

    Update title and restore font sizes

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "切って、繋いで、ぐるぐるぐるぐる",
+    pageTitle: "切って、繋いで、ぐるぐるぐる",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
